3968aaffea7b1e90643f8be3c0690c9c11931cce
marcusant <mstojcevich@gmail.com>
2015-10-03 17:52:51 -0400

    Fix indentation problems

--- src/org/usfirst/frc/team2586/robot/commands/CloseClaw.java
+++ src/org/usfirst/frc/team2586/robot/commands/CloseClaw.java
@@ -5,35 +5,36 @@ import org.usfirst.frc.team2586.robot.subsystems.Claw;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class CloseClaw extends Command {
-	
+
 	// How long, in seconds, it takes to close the claw
 	private static final double DURATION = 2;
-	
+
 	private Claw claw;
 
-    public CloseClaw(Claw claw) {
-    	this.claw = claw;
-    }
+	public CloseClaw(Claw claw) {
+		this.claw = claw;
+	}
 
 	@Override
-    protected void initialize() {
+	protected void initialize() {
 		this.claw.closeClaw();
-    }
+	}
 
-    @Override
-    protected void execute() {
-    }
+	@Override
+	protected void execute() {
+	}
 
-    @Override
-    protected boolean isFinished() {
-        return this.timeSinceInitialized() > DURATION;
-    }
+	@Override
+	protected boolean isFinished() {
+		return this.timeSinceInitialized() > DURATION;
+	}
 
-    @Override
-    protected void end() {
-    }
+	@Override
+	protected void end() {
+	}
 
-    @Override
-    protected void interrupted() {
-    }
+	@Override
+	protected void interrupted() {
+	}
+	
 }
--- src/org/usfirst/frc/team2586/robot/commands/OpenClaw.java
+++ src/org/usfirst/frc/team2586/robot/commands/OpenClaw.java
@@ -6,35 +6,36 @@ import edu.wpi.first.wpilibj.command.Command;
 
 // TODO create TimedCommand abstract superclass to use for all commands with a duration
 public class OpenClaw extends Command {
-	
+
 	// How long, in seconds, it takes to open the claw
 	private static final double DURATION = 0.5;
-	
+
 	private Claw claw;
 
-    public OpenClaw(Claw claw) {
-    	this.claw = claw;
-    }
+	public OpenClaw(Claw claw) {
+		this.claw = claw;
+	}
 
 	@Override
-    protected void initialize() {
+	protected void initialize() {
 		this.claw.openClaw();
-    }
+	}
 
-    @Override
-    protected void execute() {
-    }
+	@Override
+	protected void execute() {
+	}
 
-    @Override
-    protected boolean isFinished() {
-        return this.timeSinceInitialized() > DURATION;
-    }
+	@Override
+	protected boolean isFinished() {
+		return this.timeSinceInitialized() > DURATION;
+	}
 
-    @Override
-    protected void end() {
-    }
+	@Override
+	protected void end() {
+	}
 
-    @Override
-    protected void interrupted() {
-    }
+	@Override
+	protected void interrupted() {
+	}
+	
 }
