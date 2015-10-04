227b6cdf8b42dc5db799ba4181f7b43a06962484
marcusant <mstojcevich@gmail.com>
2015-10-04 14:14:09 -0400

    Allow more abstract use of DriveTrain (different speed controllers, different channels

--- src/org/usfirst/frc/team2586/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2586/robot/subsystems/DriveTrain.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team2586.robot.subsystems;
 import org.usfirst.frc.team2586.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,20 +12,50 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class DriveTrain extends Subsystem {
 	
-	private final Talon leftRearDriveMotor;
-	private final Talon leftFrontDriveMotor;
-	private final Talon rightRearDriveMotor;
-	private final Talon rightFrontDriveMotor;
+	private final SpeedController leftRearDriveMotor;
+	private final SpeedController leftFrontDriveMotor;
+	private final SpeedController rightRearDriveMotor;
+	private final SpeedController rightFrontDriveMotor;
 	
 	private final RobotDrive drive;
 	
+	/**
+	 * Create a DriveTrain using the default speed controller channels defined in RobotMap as Talons.
+	 */
 	public DriveTrain() {
+		this(RobotMap.REAR_LEFT_DRIVE, RobotMap.REAR_RIGHT_DRIVE,
+				RobotMap.FRONT_LEFT_DRIVE, RobotMap.FRONT_RIGHT_DRIVE);
+	}
+	
+	/**
+	 * Create a DriveTrain using the specified speed controller channels using Talons
+	 * 
+	 * @param rearLeft Channel for the rear left speed controller
+	 * @param rearRight Channel for the rear right speed controller
+	 * @param frontLeft Channel for the front left speed controller
+	 * @param frontRight Channel for the front right speed controller
+	 */
+	public DriveTrain(int rearLeft, int rearRight, int frontLeft, int frontRight) {
+		this(new Talon(rearLeft), new Talon(rearRight), 
+				new Talon(frontLeft), new Talon(frontRight));
+	}
+	
+	/**
+	 * Create a DriveTrain using the spcified speed controllers
+	 * 
+	 * @param rearLeft Rear left speed controller
+	 * @param rearRight Rear right speed controller
+	 * @param frontLeft Front left speed controller
+	 * @param frontRight Front right speed controller
+	 */
+	public DriveTrain(SpeedController rearLeft, SpeedController rearRight, 
+			SpeedController frontLeft, SpeedController frontRight) {
 		super("Drive Train");
 		
-		this.leftRearDriveMotor = new Talon(RobotMap.REAR_LEFT_DRIVE);
-		this.leftFrontDriveMotor = new Talon(RobotMap.FRONT_LEFT_DRIVE);
-		this.rightRearDriveMotor = new Talon(RobotMap.REAR_RIGHT_DRIVE);
-		this.rightFrontDriveMotor = new Talon(RobotMap.FRONT_RIGHT_DRIVE);
+		this.leftRearDriveMotor = rearLeft;
+		this.rightRearDriveMotor = rearRight;
+		this.leftFrontDriveMotor = frontLeft;
+		this.rightFrontDriveMotor = frontRight;
 		
 		this.drive = new RobotDrive(
 				leftFrontDriveMotor, leftRearDriveMotor,
@@ -44,8 +75,7 @@ public class DriveTrain extends Subsystem {
 	}
 
 	@Override
-	protected void initDefaultCommand() {
-		
-	}
+	protected void initDefaultCommand() {}
+	
 }
 
